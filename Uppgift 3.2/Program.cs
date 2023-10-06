e1b0ac2689c3544a0e86e08d56e6be80bb75539b
Dijar06 <dize06001@utb.vaxjo.se>
2023-10-06 12:43:38 +0200

    Add project files.

---
+++ Uppgift 3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej, vi anställer! Har du gått ut ur gymnasiet? Svara enligt följande:");
+            Console.WriteLine("j = Ja, n = Nej");
+            string student = Console.ReadLine().ToLower();
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (student == "j" && ålder < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig!");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu.");
+            }
+        }
+    }
+}
